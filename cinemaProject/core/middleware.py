a48c14f0c475f31b0918875e50780711e8e1f648
ssava <santa03777@gmail.com>
2024-04-07 02:11:50 +0300

    autologout fixes

--- cinemaProject/core/middleware.py
+++ cinemaProject/core/middleware.py
@@ -7,13 +7,12 @@ from django.utils.deprecation import MiddlewareMixin
 class AutoLogoutMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.user.is_authenticated:
-            last_activity = request.session.get('last_activity')
-            print(last_activity)
-            last_activity_from_iso = datetime.fromisoformat(last_activity)
-            if last_activity_from_iso and datetime.now() - last_activity_from_iso > timedelta(seconds=10):
-                logout(request)
-                return redirect('/login/')
-            else:
-                current_time_iso = datetime.now().isoformat()
-                print(current_time_iso)
-                request.session['last_activity'] = current_time_iso
+            if not request.user.is_staff:
+                last_activity = request.session.get('last_activity')
+                last_activity_from_iso = datetime.fromisoformat(last_activity)
+                if last_activity_from_iso and datetime.now() - last_activity_from_iso > timedelta(seconds=10):
+                    logout(request)
+                    return redirect('/login/')
+                else:
+                    current_time_iso = datetime.now().isoformat()
+                    request.session['last_activity'] = current_time_iso
